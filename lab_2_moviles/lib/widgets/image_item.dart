1d8da305bd4f59be9d71963b367a2623b8a5113c
DanielGCh1 <dgchaarturo@gmail.com>
2023-05-26 22:41:33 -0600

    animacion imagenes

--- lab_2_moviles/lib/widgets/image_item.dart
+++ lab_2_moviles/lib/widgets/image_item.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
-class ImageItem extends StatelessWidget {
+class ImageItem extends StatefulWidget {
   final String imageUrl;
   final bool isScrollingDown;
   final int crossAxisCount;
@@ -12,29 +12,72 @@ class ImageItem extends StatelessWidget {
     required this.isScrollingDown,
     required this.crossAxisCount,
     required this.index,
-  });
+    Key? key,
+  }) : super(key: key);
 
-  static const animationDuration = Duration(milliseconds: 500);
+  @override
+  ImageItemState createState() => ImageItemState();
+}
+
+class ImageItemState extends State<ImageItem>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500),
+    );
+
+    // Iniciar la animación
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  // Método para iniciar la animación de desaparición de la imagen
+  void startDisappearAnimation() {
+    _animationController.reverse();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final borderRadius = BorderRadius.circular(8.0);
-
     return AnimationConfiguration.staggeredGrid(
-      position: index,
-      columnCount: crossAxisCount,
-      duration: animationDuration,
-      child: ScaleAnimation(
-        child: FadeInAnimation(
-          child: SlideAnimation(
-            verticalOffset: isScrollingDown ? 50.0 : -50.0,
-            child: Container(
-              margin: EdgeInsets.all(8.0),
-              child: ClipRRect(
-                borderRadius: borderRadius,
-                child: Image.network(
-                  imageUrl,
-                  fit: BoxFit.cover,
+      position: widget.index,
+      columnCount: widget.crossAxisCount,
+      duration: const Duration(milliseconds: 1500),
+      child: ScaleTransition(
+        scale: CurvedAnimation(
+          parent: _animationController,
+          curve: Interval(0.0, 1.0, curve: Curves.easeOut),
+        ),
+        child: FadeTransition(
+          opacity: _animationController,
+          child: SlideTransition(
+            position: Tween<Offset>(
+              begin:
+                  widget.isScrollingDown ? Offset(0.0, 0.5) : Offset(0.0, -0.5),
+              end: Offset.zero,
+            ).animate(_animationController),
+            child: SizeTransition(
+              sizeFactor: CurvedAnimation(
+                parent: _animationController,
+                curve: Interval(0.0, 1.0, curve: Curves.easeOut),
+              ),
+              child: Container(
+                margin: EdgeInsets.all(8.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: Image.network(
+                    widget.imageUrl,
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
             ),
